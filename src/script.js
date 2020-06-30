c0a5bad8b7b2ffa3547ddd6724575bf32afb7312
Evgenii <64902923+ZGuzhov@users.noreply.github.com>
2020-06-30 15:41:25 +0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -4,15 +4,6 @@ const city_form = document.querySelector('.form-group')
 const btn_city = document.querySelector('.btn-city')
 const btn_city_clr = document.querySelector('.btn-city-clr')
 
-const btn_checkbox = document.querySelector('.btn-checkbox')
-const ch1 = document.querySelector('input[id=Check1]')
-const ch2 = document.querySelector('input[id=Check2]')
-const ch3 = document.querySelector('input[id=Check3]')
-const ch4 = document.querySelector('input[id=Check4]')
-const ch5 = document.querySelector('input[id=Check5]')
-const ch6 = document.querySelector('input[id=Check6]')
-
-
 btn_city.onclick = () =>{
 	if (city.value != "") {
 		document.cookie = "city=" + city.value
@@ -58,6 +49,14 @@ if (city_cookie) {
 	city.value = ""	
 }
 
+const btn_checkbox = document.querySelector('.btn-checkbox')
+const ch1 = document.querySelector('input[id=Check1]')
+const ch2 = document.querySelector('input[id=Check2]')
+const ch3 = document.querySelector('input[id=Check3]')
+const ch4 = document.querySelector('input[id=Check4]')
+const ch5 = document.querySelector('input[id=Check5]')
+const ch6 = document.querySelector('input[id=Check6]')
+
 btn_checkbox.onclick = () =>{
 	let key = ""
 	if (ch1.checked) {key="1"} else {key="0"}
@@ -79,12 +78,12 @@ btn_checkbox.onclick = () =>{
 let ch_cookie = getCookie("ch")
 
 if (ch_cookie) {
-	if (ch_cookie[0] === "0") {ch1.checked = False} else {ch1.checked = True}
-	if (ch_cookie[1] === "0") {ch2.checked = False} else {ch2.checked = True}
-	if (ch_cookie[2] === "0") {ch3.checked = False} else {ch3.checked = True}
-	if (ch_cookie[3] === "0") {ch4.checked = False} else {ch4.checked = True}
-	if (ch_cookie[4] === "0") {ch5.checked = False} else {ch5.checked = True}
-	if (ch_cookie[5] === "0") {ch6.checked = False} else {ch6.checked = True}
+	if (ch_cookie[0] === "0") {ch1.checked = false} else {ch1.checked = true}
+	if (ch_cookie[1] === "0") {ch2.checked = false} else {ch2.checked = true}
+	if (ch_cookie[2] === "0") {ch3.checked = false} else {ch3.checked = true}
+	if (ch_cookie[3] === "0") {ch4.checked = false} else {ch4.checked = true}
+	if (ch_cookie[4] === "0") {ch5.checked = false} else {ch5.checked = true}
+	if (ch_cookie[5] === "0") {ch6.checked = false} else {ch6.checked = true}
 	btn_checkbox.style.cssText = "display: none;"
 	ch1.disabled = true
 	ch2.disabled = true
